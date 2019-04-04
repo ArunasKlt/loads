2ffd55ffa3cfc5256d4715e44c272f38c322a287
SANTA\arukal <kalitovicius.arunas@yahoo.com>
2019-04-04 15:56:20 +0300

    New cargo php page

---
+++ newcargo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	Siuntėjas: <input type="text" name="siuntejas"><br>
+	Gavėjas: <input type="text" name="gavejas"><br>
+	Iškrovimo adresas: <input type="text" name="iskrovimas"><br>
+	Iškrovimo šalis: <input type="text" name="gavejas"><br>
+	EPL: <input type="radio" name="load" value="EPL">
+	FIN: <input type="radio" name="load" value="FIN">
+	IND: <input type="radio" name="load" value="IND">
+	DRUM: <input type="radio" name="load" value="DRUM"><br><br>
+	Ilgis: <input type="text" name="ilgis">
+	Plotis: <input type="text" name="plotis">
+	Aukštis: <input type="text" name="aukstis">
+	Svoris: <input type="text" name="ilgis"> t.<br><br>
+	Kiekis: <input type="text" name="kiekis">
+
+</form>
+
+</body>
+</html>
